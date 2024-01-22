277cd5e35cee9e44e417146c5debb793e78bbbee
Sasha Bogolii <sbogolii@siftscience.com>
2024-01-22 17:17:38 +0200

    Added simulations default, est and eft

--- src/main/java/ua/edu/krok/Main.java
+++ src/main/java/ua/edu/krok/Main.java
@@ -8,6 +8,7 @@ import ua.edu.krok.scheduler.TaskBoard;
 import ua.edu.krok.scheduler.TaskSet;
 import ua.edu.krok.scheduler.Team;
 import ua.edu.krok.scheduler.impl.DAGProject;
+import ua.edu.krok.scheduler.impl.DefaultSimulator;
 import ua.edu.krok.scheduler.impl.DefaultTeam;
 import ua.edu.krok.scheduler.impl.DefaultTeamMember;
 import ua.edu.krok.scheduler.impl.ESTSimulator;
@@ -21,29 +22,33 @@ public class Main {
         TaskSet<TimedTask> taskSet = generator.generate(10, 10, 8);
 
 
-        System.out.println("---EST of Distributed team---");
+        System.out.println("---Default Simulator of Distributed team---");
         Team team = new DefaultTeam();
-        team.addTeamMember(new DefaultTeamMember(1));
-        team.addTeamMember(new DefaultTeamMember(2));
+        LocalDateTime now = LocalDateTime.now();
+        team.addTeamMember(new ScheduleAwareTeamMember(1,
+            ZoneId.of("Europe/Kiev").getRules().getOffset(now), 10));
+        team.addTeamMember(
+            new ScheduleAwareTeamMember(2,
+                ZoneId.of("America/Los_Angeles").getRules().getOffset(now), 9));
 
-        Simulator<TimedTask> simulator1 = new ESTSimulator();
+        Simulator<TimedTask> simulator1 = new DefaultSimulator();
         TaskBoard taskBoard1 = simulator1.simulate(new DAGProject<>(taskSet), team);
 
         System.out.println(taskBoard1);
 
         System.out.println("---EST (timezone aware) of Distributed team---");
 
-        Team timeZoneAwareTeam = new DefaultTeam();
-        LocalDateTime now = LocalDateTime.now();
-        timeZoneAwareTeam.addTeamMember(new ScheduleAwareTeamMember(1,
-            ZoneId.of("Europe/Kiev").getRules().getOffset(now), 10));
-        timeZoneAwareTeam.addTeamMember(
-            new ScheduleAwareTeamMember(2,
-                ZoneId.of("America/Los_Angeles").getRules().getOffset(now), 9));
 
         Simulator<TimedTask> simulator2 = new ESTSimulator();
-        TaskBoard taskBoard2 = simulator2.simulate(new DAGProject<>(taskSet), timeZoneAwareTeam);
+        TaskBoard taskBoard2 = simulator2.simulate(new DAGProject<>(taskSet), team);
 
         System.out.println(taskBoard2);
+
+        System.out.println("---EFT (timezone aware) of Distributed team---");
+
+        Simulator<TimedTask> simulator3 = new ESTSimulator();
+        TaskBoard taskBoard3 = simulator3.simulate(new DAGProject<>(taskSet), team);
+
+        System.out.println(taskBoard3);
     }
 }
